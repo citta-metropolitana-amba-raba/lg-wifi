a868b81bb79226424192b3ac6cb7fa5e8ef86bb5
Alberto Torin <alberto.torin@gmail.com>
2019-02-07 16:32:18 +0100

    Test documento

---
+++ conf.py
@@ -0,0 +1,10 @@
+import docs_italia_theme
+
+extensions = [
+    'docs_italia_theme'
+]
+
+master_doc = 'index'
+html_theme = 'docs_italia_theme'
+html_theme_path = [docs_italia_theme.get_html_theme_path()]
+numfig = True
